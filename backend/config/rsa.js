113c45057c813620e238073bee098d4975407953
baitieyi <baitieyi@163.com>
2019-05-16 23:06:51 +0800

    add rsa config file

---
+++ backend/config/rsa.js
@@ -0,0 +1,8 @@
+const publicKey=`-----BEGIN PUBLIC KEY-----
+-----END PUBLIC KEY-----`;
+const privateKey=`-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----`;
+module.exports = {
+  publicKey,
+  privateKey
+};
