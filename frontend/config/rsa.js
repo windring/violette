aaa3ab16a18435741b4df46796794bd6ab4e0c86
baitieyi@Product <baitieyi@163.com>
2019-05-31 00:01:51 +0800

    delete it

--- frontend/config/rsa.js
+++
@@ -1,8 +0,0 @@
-const publicKey=`-----BEGIN PUBLIC KEY-----
------END PUBLIC KEY-----`;
-const privateKey=`-----BEGIN RSA PRIVATE KEY-----
------END RSA PRIVATE KEY-----`;
-module.exports = {
-  publicKey,
-  privateKey
-};
